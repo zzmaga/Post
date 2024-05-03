0f63d80d80dbb786bbd574da1c0cd743e0e37ca9
Magzhan <magjantastanov@gmail.com>
2024-05-03 14:48:02 +0500

    Update whitenoise

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -102,7 +102,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStoragge' \
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' \
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
